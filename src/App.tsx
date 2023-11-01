f551986599ff02141dd30603514281a5cf9ba989
gabriel leal santos <gabriel.lealsantos@hotmail.com>
2023-11-01 10:33:41 -0300

    Not found route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientHome from "./routes/ClientHome";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProudctDetails from "./routes/ClientHome/ProductDetails";
@@ -17,6 +17,7 @@ export default function App() {
           <Route path="catalog" element={<Catalog />} />
           <Route path="product-details/:productId" element={<ProudctDetails />} />
         </Route>
+        <Route path="*" element={<Navigate to="/"/>}/>
       </Routes>
     </BrowserRouter>
   )
